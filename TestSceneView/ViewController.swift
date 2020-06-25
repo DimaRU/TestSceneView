fb485236ef1eb6add3676e1ef61720f8aee25fc7
Dmitriy Borovikov <dmitry@borovikov.name>
2020-06-25 10:47:51 +0300

    Initial commit

---
+++ TestSceneView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSceneView
+//
+//  Created by Dmitriy on 29.05.2020.
+//  Copyright © 2020 Dmitriy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
